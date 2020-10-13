9fef5fa98fce34fc67986dffd243806a49b74f0d
Strömberg Viggo <viggo.stromberg@skola.uppsala.se>
2020-10-13 21:11:50 +0200

    stdlib

--- hello.asm
+++ hello.asm
@@ -18,22 +18,57 @@ _print:
 	pop rbp
 	ret
 
-_digitToChar:
+_printInt:
 	push rbp
 	mov rbp, rsp
-	sub rsp, 16
-	mov rax, 48
-	mov qword [rbp - 8], rax
+	xor rdx, rdx
+	mov rax, rsp
+	mov r8, 16
+	div r8
+	sub rsp, rdx
 	mov rax, qword [rbp + 16]
-	mov qword [rbp - 16], rax
-	mov rax, qword [rbp + 24]
-	mov rcx, rax
-	mov rax, qword [rbp - 16]
-	add rax, rcx
-	mov rcx, rax
-	mov rax, qword [rbp - 8]
-	add rax, rcx
-.ret:
+	cmp rax, 0
+	je .zero
+	jmp .nozero
+.zero:
+	mov rcx, 48
+	sub rsp, 40
+	call putchar
+	jmp .ret1
+.nozero:
+	jl .neg
+	jmp .noneg
+.neg:
+	mov rcx, 45
+	push rax
+	sub rsp, 32
+	call putchar
+	add rsp, 32
+	pop rax
+	neg rax
+.noneg:
+	sub rsp, 8
+	call .loop
+.ret1:
+	mov rsp, rbp
+	pop rbp
+	ret
+.loop:
+	push rbp
+	mov rbp, rsp
+	cmp rax, 0
+	je .ret2
+	xor rdx, rdx
+	mov rcx, 10
+	div rcx
+	push rdx
+	call .loop
+	pop rdx
+	mov rcx, rdx
+	add rcx, 48
+	sub rsp, 40
+	call putchar
+.ret2:
 	mov rsp, rbp
 	pop rbp
 	ret
@@ -41,15 +76,64 @@ _digitToChar:
 main:
 	push rbp
 	mov rbp, rsp
-	sub rsp, 0
-	sub rsp, 8
 	sub rsp, 16
-	mov rax, 3
+	sub rsp, 8
+	mov rax, 69
 	mov qword [rsp + 0], rax
+	call _printInt
+	add rsp, 8
+	sub rsp, 8
+	mov rax, 10
+	mov qword [rsp + 0], rax
+	call _print
+	add rsp, 8
+	sub rsp, 8
 	mov rax, 1
-	mov qword [rsp + 8], rax
-	call _digitToChar
-	add rsp, 16
+	mov qword [rsp + 0], rax
+	call _printInt
+	add rsp, 8
+	sub rsp, 8
+	mov rax, 10
+	mov qword [rsp + 0], rax
+	call _print
+	add rsp, 8
+	sub rsp, 8
+	mov rax, 0
+	mov qword [rsp + 0], rax
+	call _printInt
+	add rsp, 8
+	sub rsp, 8
+	mov rax, 10
+	mov qword [rsp + 0], rax
+	call _print
+	add rsp, 8
+	sub rsp, 8
+	xor rax, rax
+	mov qword [rbp - 8], rax
+	mov rax, 1
+	mov rcx, rax
+	mov rax, qword [rbp - 8]
+	sub rax, rcx
+	mov qword [rsp + 0], rax
+	call _printInt
+	add rsp, 8
+	sub rsp, 8
+	mov rax, 10
+	mov qword [rsp + 0], rax
+	call _print
+	add rsp, 8
+	sub rsp, 8
+	xor rax, rax
+	mov qword [rbp - 16], rax
+	mov rax, 69
+	mov rcx, rax
+	mov rax, qword [rbp - 16]
+	sub rax, rcx
+	mov qword [rsp + 0], rax
+	call _printInt
+	add rsp, 8
+	sub rsp, 8
+	mov rax, 10
 	mov qword [rsp + 0], rax
 	call _print
 	add rsp, 8
