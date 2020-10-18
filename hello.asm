ea2b6727dad0a757314c426c486067c708d8b7e9
viklo <viggo@gstromberg.nu>
2020-10-18 18:02:48 +0200

    fixade två subtila buggar

---
+++ hello.asm
@@ -0,0 +1,100 @@
+	global main
+	extern ExitProcess
+	extern putchar
+
+section .text
+_printInt:
+	push rbp
+	mov rbp, rsp
+	xor rdx, rdx
+	mov rax, rsp
+	mov r8, 16
+	div r8
+	sub rsp, rdx
+	mov rax, qword [rbp + 16]
+	cmp rax, 0
+	je .zero
+	jmp .nozero
+.zero:
+	mov rcx, 48
+	sub rsp, 40
+	call putchar
+	jmp .ret1
+.nozero:
+	jl .neg
+	jmp .noneg
+.neg:
+	mov rcx, 45
+	push rax
+	sub rsp, 32
+	call putchar
+	add rsp, 32
+	pop rax
+	neg rax
+.noneg:
+	sub rsp, 8
+	call .loop
+.ret1:
+	mov rsp, rbp
+	pop rbp
+	ret
+.loop:
+	push rbp
+	mov rbp, rsp
+	cmp rax, 0
+	je .ret2
+	xor rdx, rdx
+	mov rcx, 10
+	div rcx
+	push rdx
+	call .loop
+	pop rdx
+	mov rcx, rdx
+	add rcx, 48
+	sub rsp, 40
+	call putchar
+.ret2:
+	mov rsp, rbp
+	pop rbp
+	ret
+
+_add:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	xor rax, rax
+	mov qword [rbp - 8], rax
+	mov rax, qword [rbp + 16]
+	mov rcx, rax
+	mov rax, qword [rbp - 8]
+	sub rax, rcx
+	mov qword [rbp - 16], rax
+	mov rax, 3
+	mov rcx, rax
+	mov rax, qword [rbp - 16]
+	imul rax, rcx
+	jmp .ret
+.ret:
+	mov rsp, rbp
+	pop rbp
+	ret
+
+main:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 0
+	sub rsp, 8
+	sub rsp, 8
+	mov rax, 10
+	mov qword [rsp + 0], rax
+	call _add
+	add rsp, 8
+	mov qword [rsp + 0], rax
+	call _printInt
+	add rsp, 8
+.ret:
+	mov rsp, rbp
+	pop rbp
+	xor rcx, rcx
+	call ExitProcess
+
